6f3efe00e5da73c28a8240d2040fa97f2502d9dd
Andrew Bruce <andrew@camelpunch.com>
2013-02-17 22:24:44 -0800

    move queue specs to spec/integration

---
+++ spec/integration/resque_queue_spec.rb
@@ -0,0 +1,12 @@
+require_relative '../spec_helper'
+require_relative '../support/shared_examples/queues.rb'
+require_relative '../../lib/plumb/resque_queue'
+
+module Plumb
+  class ResqueQueueSpec < SpecSupport::QueueSpec
+    def queue_named(name)
+      ResqueQueue.new(name)
+    end
+  end
+end
+

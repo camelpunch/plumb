a241669aca29d617acebcbbc8b04e487d28a65f0
Andrew Bruce <andrew@camelpunch.com>
2013-04-06 23:03:51 -0700

    shuffle acceptance test in prep for real exec

--- spec/acceptance_spec.rb
+++ spec/acceptance_spec.rb
@@ -8,9 +8,8 @@ describe "plumb" do
 
   it "shows various statuses for a build's lifecycle in the CCTray feed" do
     Dir.mktmpdir do |dir|
-      bin_path = Pathname(File.expand_path('../../bin', __FILE__))
-      config_path = Pathname(dir)
-      IO.write config_path.join('ci.yml'), Psych.dump(
+      config_path = "#{dir}/ci.yml"
+      IO.write config_path, Psych.dump(
         'server' => {
           'adapter' => ['rack', 'Plumb::Server'],
           'endpoint' => 'http://some.url/'
@@ -18,29 +17,31 @@ describe "plumb" do
         'projects' => {
           "happy_project" => {
             'name' => 'Happy Project',
-            'script' => 'rake',
+            'script' => "> '#{dir}/created_file'",
             'repository_url' => 'git://foo.bar'
           }
         }
       )
-      run_command_in_this_thread(
-        bin_path.join('plumb-configure'),
-        config_path.join('ci.yml')
-      )
+      run_command_in_this_thread('plumb-configure', config_path)
+
+      # start build
+      put_build 'happy_project', 'some-build-id', status: 'Building'
+      project_activity('Happy Project').must_equal 'Building'
+
+      # finish build
+      get_dashboard
+      build_id = last_build_label_for_project('Happy Project')
+      put_build 'happy_project', build_id, status: 'Success'
+
+      project_activity('Happy Project').must_equal 'Sleeping'
     end
-
-    put_build 'happy_project', 'some-build-id', status: 'Building'
-    project_activity('Happy Project').must_equal 'Building'
-
-    get_dashboard
-    build_id = last_build_label_for_project('Happy Project')
-    put_build 'happy_project', build_id, status: 'Success'
-
-    project_activity('Happy Project').must_equal 'Sleeping'
   end
 
-  def run_command_in_this_thread(path, first_arg)
-    ARGV[0] = first_arg
-    load path
+  def run_command_in_this_thread(filename, *args)
+    ARGV.clear
+    args.each do |arg|
+      ARGV << arg
+    end
+    load File.expand_path("../../bin/#{filename}", __FILE__)
   end
 end
